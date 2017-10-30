47eb806df0747246e133ef619dd404a38417fe95
aleksandr_lytysov <aleksandr.lytysov@gmail.com>
2017-10-30 15:30:53 +0700

    add seeder

--- Billing/Data/DbInitializer.cs
+++ Billing/Data/DbInitializer.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Billing.Models;
 using SQLitePCL;
 
 namespace Billing.Data
@@ -12,5 +16,95 @@ namespace Billing.Data
 
            
         }
+
+        public static async Task Seed(MyContext serviceProvider)
+        {
+            await SeedBills(serviceProvider);
+            await SeedTransactions(serviceProvider);
+        }
+
+        private static async Task SeedBills(MyContext dbContext)
+        {
+            if (!dbContext.CurrencyBills.Any())
+            {
+                await dbContext.CurrencyBills.AddRangeAsync(new List<CurrencyBill>
+                {
+                    new CurrencyBill() {
+                        Currency = "BTC",
+                        Amount = 100000,
+                        Available = 50000,
+                        OrdersAmount = 30000,
+                        AppendingAmount = 30000,
+                        DecommissioningAmount = 25000
+                    },
+                    new CurrencyBill() {
+                        Currency = "ETH",
+                        Amount = 200000,
+                        Available = 100000,
+                        OrdersAmount = 60000,
+                        AppendingAmount = 60000,
+                        DecommissioningAmount = 60000
+                    },
+                    new CurrencyBill() {
+                        Currency = "LTC",
+                        Amount = 50000,
+                        Available = 25000,
+                        OrdersAmount = 16000,
+                        AppendingAmount = 14000,
+                        DecommissioningAmount = 12500
+                    },
+                    new CurrencyBill() {
+                        Currency = "DSH",
+                        Amount = 200000,
+                        Available = 25000,
+                        OrdersAmount = 60000,
+                        AppendingAmount = 15000,
+                        DecommissioningAmount = 25000
+                    },
+                    new CurrencyBill() {
+                        Currency = "NMC",
+                        Amount = 100000,
+                        Available = 50000,
+                        OrdersAmount = 30000,
+                        AppendingAmount = 30000,
+                        DecommissioningAmount = 25000
+                    }
+                });
+            }
+
+            await dbContext.SaveChangesAsync();
+        }
+
+
+        public static async Task SeedTransactions(MyContext dbContext)
+        {
+            var transactions = new List<Transaction>
+                {
+                new Transaction(){
+                        DateTime = DateTime.Now,
+                        CurrencyName = "BTC",
+                        Amount = 2.5,
+                        TransactionStatus = TransactionStatus.Appended,
+                        ConfirmationsCount = 25
+                    },
+                new Transaction(){
+                        DateTime = DateTime.Now,
+                        CurrencyName = "ETH",
+                        Amount = 3.5,
+                        TransactionStatus = TransactionStatus.Appended,
+                        ConfirmationsCount = 250
+                    },
+                new Transaction(){
+                        DateTime = DateTime.Now,
+                        CurrencyName = "LTC",
+                        Amount = 40,
+                        TransactionStatus = TransactionStatus.Appended,
+                        ConfirmationsCount = 2500
+                    }
+                };
+
+            await dbContext.SaveChangesAsync();
+        }
+
     }
 }
